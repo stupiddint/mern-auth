3c198d36c5616c3ab781b497217cad7ffd11a9f5
naveen <stupiddint@gmail.com>
2023-02-22 22:06:41 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+// import all components
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+/** root routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/password',
+    element : <Password />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  },
+  {
+    path: '/recovery',
+    element: <Recovery />
+  },
+  {
+    path: '/Reset',
+    element : <Reset />
+  },
+  {
+    path: '*',
+    element: <PageNotFound />
+  }
+])
 function App() {
   return (
-    <div className="App">
-      fronted login app
-    
-    </div>
+    <>
+    <RouterProvider router={router}></RouterProvider>
+
+    </>
   );
 }
 
